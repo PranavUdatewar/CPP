0d0a6fdb992852db12f14d69731cbc983233cb73
Pranav Udatewar <81368045+PranavUdatewar@users.noreply.github.com>
2021-03-26 15:03:26 +0530

    Add files via upload

---
+++ Upper&LOwer.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"Upper limit and Lower limit";
+    cout<<"____________________________";
+    cout<<"The max. limit of int data type:"<<INT_MAX<<endl;
+    cout<<"The min. limit of int data type:"<<INT_MIN<<endl;
+    cout<<"The max. limit of unsigned int data type:"<<UINT_MAX<<endl;
+    cout<<"The max. limit of long long data type:"<<LLONG_MAX<<endl;
+    cout<<"The min. limit of long long data type:"<<LLONG_MIN<<endl;
+    cout<<"The max. limit of unsigned long long data type:"<<ULLONG_MAX<<endl;
+    cout<<"The Bits contain in char data type:"<<CHAR_BIT<<endl;
+    cout<<"The max. limit of char data type:"<<CHAR_MAX<<endl;
+    cout<<"The min. limit of char data type:"<<CHAR_MIN<<endl;
+    cout<<"The max. limit of signed char data type:"<<SCHAR_MAX<<endl;
+    cout<<"The min. limit of signed char data type:"<<SCHAR_MIN<<endl;
+    cout<<"The max. limit of unsigned char data type:"<<UCHAR_MAX<<endl;
+    cout<<"The max. limit of short data type:"<<SHRT_MAX<<endl;
+    cout<<"The min. limit of short data type:"<<SHRT_MIN<<endl;
+    cout<<"The max. limit of unsigned short data type:"<<USHRT_MAX<<endl;
+
+}
