b06ac79286e32ea7d9424ef65bb12645c9e22881
Pranav Udatewar <81368045+PranavUdatewar@users.noreply.github.com>
2021-03-26 15:03:55 +0530

    Add files via upload

---
+++ sizeof.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std ;
+int main()
+{
+    cout<<"Sizeof(char): "<<sizeof(char)<<" byte \n";
+    cout<<"Sizeof(int): "<<sizeof(int)<<" byte \n";
+    cout<<"Sizeof(short): "<<sizeof(short)<<" byte \n";
+    cout<<"Sizeof(long): "<<sizeof(long)<<" byte \n";
+    cout<<"Sizeof(double): "<<sizeof(double)<<" byte \n";
+}
