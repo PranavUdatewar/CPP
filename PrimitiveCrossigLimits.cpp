669ae8b8a4f972ce05f999b02e1300442a751641
Pranav Udatewar <81368045+PranavUdatewar@users.noreply.github.com>
2021-03-27 22:31:26 +0530

    Add files via upload

---
+++ PrimitiveCrossigLimits.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char gender ='F';
+    bool married =true;
+    int son =2;
+    int appointment =2009;
+    int salary =1500000;
+    float height =79.48;
+    float gpa =4.69;
+    int drawn =12047235;
+    int balance =995324987;
+    cout<<"Check whether the primitive values crossing the limits\n ";
+    cout<<"---------------------------------------------------------\n";
+    cout<<"The Gender is:"<<gender<<endl;
+    cout<<"Is she married?:"<<married<<endl;
+    cout<<"Number of sons:"<<son<<endl;
+    cout<<"Year of appointment:"<<appointment<<endl;
+    cout<<"Salary for year:"<<salary<<endl;
+    cout<<"Height is:"<<height<<endl;
+    cout<<"GPA is:"<<gpa<<endl;
+    cout<<"Salary drawn upto:"<<drawn<<endl;
+    cout<<"Balance till:"<<balance<<endl;
+}
